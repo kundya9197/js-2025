7811ece05a692df1b8a6b7f0cfb6dd7c6401aace
Kundan <96116488+kundya9197@users.noreply.github.com>
2025-01-02 20:35:50 +0000

    postIncrement and preIncrement

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+console.log("PostIncrement")
+let x = 3;
+const  y = x++
+console.log(`x:${x}, y:${y}`);
+// below and above outputs both are same just syntax is changed
+// console.log("x:" + x + ", " + "y:" + y)  
+console.log(x);
+
+// PreIncrement
+// let a = 3
+// const b = ++a;
+// console.log(b)
+// console.log(`a:${b}, b:${b}`)
