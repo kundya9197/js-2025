f193f6145805501f7b659ea9f1863c1df5b037a0
Kundan <96116488+kundya9197@users.noreply.github.com>
2025-01-05 21:06:50 +0000

    arrays basics

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+// array
+// index starts from 0
+// typeof array is object
+// Numbers/Strings present in arrays are called elements
+// arrays can be of mixed type includes boolean values, numbers and strings
+// arrays in javascript are resizable/
+// we can add elements into it after declare it 
+// it can be of mixed different Datatype(String,Numbers...)
+
+// creating an number type array
+const myArr = [0, 1, 2, 3, 4, 5]
+console.log(typeof(myArr));
+
+// another way to declare array
+const myArr2 = new Array(1, 2, 3, 4, 5)
