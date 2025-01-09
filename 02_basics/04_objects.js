fd3fae007f72c0a473ac099d24e570ae2b8e7d05
Kundan <96116488+kundya9197@users.noreply.github.com>
2025-01-09 12:54:46 +0000

    seperating and merging objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,72 @@
+// creating object with construtor
+// both tinderUser returns empty object {}
+// const tinderUser = new Object()// this is singleton
+const tinderUser = {} //normal object
+// giving values to the object
+tinderUser.id = "123abc"
+tinderUser.name = "kundan"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+// creating nested object
+const regularUser = {
+    email:"kundanmahajan18@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"kundan",
+            lastname:"mahajan"
+        }
+    }
+}
+// accessing values of above nested object
+// console.log(regularUser.fullname);
+// console.log(regularUser.fullname.userfullname);
+// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.lastname);
+
+// merging objects
+//  const obj1 = {1:"a",2:"b"} 
+//  const obj2 = {3:"a",4:"b"} 
+//  const obj3 = {obj1,obj2}
+//  console.log(obj3);
+// under one object it returns two objects like: 
+// { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// merging object in another way
+// assign() method it have two parameters .target(target,source)
+// const obj3 = Object.assign(obj1,obj2) // both print same result
+// const obj4 = Object.assign({},obj1,obj2,obj3)  // {} are optional in this case but it's good practice to give it
+//{} means values can merge in one object
+// console.log(obj3);
+// console.log(obj4);
+
+// using spread[...] method to seperate the objects
+// const obj4 = {...obj1,...obj2}
+// console.log(obj4);
+
+// complex object values which comes from database
+// we can access them like
+const users = [
+    {
+        id:1,
+        email:"k@gmail.com"
+    },
+    {
+        id:1,
+        email:"k@gmail.com"
+    },
+    {
+        id:1,
+        email:"k@gmail.com"
+    },
+]
+users[1].name
+console.log(tinderUser);
+
+// returns an array
+console.log(Object.keys(tinderUser)); // gives keys
+console.log(Object.values(tinderUser)); // gives values
+console.log(Object.entries(tinderUser)); // converts in arrays // rarely used
+
+// to check the values present in array we use .hasOwnProperty('')
+console.log(tinderUser.hasOwnProperty('name'));
